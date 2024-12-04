21e6a5d414f65dd4ff8c45da6a55f21388f36e2b
Jeffersson <barrosjefferson407@gmail.com>
2024-12-04 16:09:23 -0300

    Serializer do app users

--- django/users/serializers.py
+++ django/users/serializers.py
@@ -1,15 +1,16 @@
-from users.models import User
 from rest_framework import serializers
+from users.models import User
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = [
-            'url',
-            'username',
-            'email',
-            'is_staff',
-            'phone_number',
-            'cpf',
-            'date_birth'
+            "url",
+            "username",
+            "email",
+            "is_staff",
+            "phone_number",
+            "cpf",
+            "date_birth",
         ]
