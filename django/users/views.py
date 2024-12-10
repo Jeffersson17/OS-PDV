59a480860cd7b39bfa9ff08be81b22e22616d126
Jeffersson <barrosjefferson407@gmail.com>
2024-12-10 17:59:58 -0300

    Criação da ListAPI e DetailAPI

--- django/users/views.py
+++ django/users/views.py
@@ -1,10 +1,18 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, viewsets
 from users.models import User
 from users.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all().order_by("id")
+    serializer_class = UserSerializer
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all().order_by("id")
+    serializer_class = UserSerializer
+
+
+class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
