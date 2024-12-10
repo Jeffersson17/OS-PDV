753c89c1ab33c7a2e0cc2ad0309072d0b09ddb6c
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2024-12-10 19:02:18 -0300

    Revert "Revert "CRUD_Sales (#6)" (#7)"
    This reverts commit 68fba39271510fb506e2256cccd1f1898338ebb7.

--- django/sales/views.py
+++ django/sales/views.py
@@ -1,13 +1,33 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 from sales.models import ProductsSales, Sales
 from sales.serializers import SerializerProductsSales, SerializerSales
 
 
 class ProductsSalesViewSet(viewsets.ModelViewSet):
-    queryset = ProductsSales.objects.all()
+    queryset = ProductsSales.objects.all().order_by("id")
     serializer_class = SerializerProductsSales
 
 
 class SalesViewSet(viewsets.ModelViewSet):
+    queryset = Sales.objects.all().order_by("id")
+    serializer_class = SerializerSales
+
+
+class SalesListAPIView(generics.ListAPIView):
+    queryset = Sales.objects.all().order_by("id")
+    serializer_class = SerializerSales
+
+
+class ProductsSalesListAPIView(generics.ListAPIView):
+    queryset = ProductsSales.objects.all().order_by("id")
+    serializer_class = SerializerProductsSales
+
+
+class SalesDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Sales.objects.all()
     serializer_class = SerializerSales
+
+
+class ProductsSalesDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductsSales.objects.all()
+    serializer_class = SerializerProductsSales
