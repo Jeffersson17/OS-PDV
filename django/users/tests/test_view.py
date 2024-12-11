ed184c25581b4678cab28186ebabb129e1b65049
Jeffersson <barrosjefferson407@gmail.com>
2024-12-11 19:08:22 -0300

    Testes Unitarios no user

---
+++ django/users/tests/test_view.py
@@ -0,0 +1,20 @@
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from users.models import User
+
+
+class UserViewSetTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = "/users/"
+        self.user1 = User.objects.create_user(
+            username="teste1",
+            email="teste1@teste.com",
+            password="teste1",
+        )
+
+    def test_list_user(self):
+        self.url_list = "/users-api/list/"
+        response = self.client.get(self.url_list)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
