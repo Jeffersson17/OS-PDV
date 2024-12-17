ce69b94bb624947b314d35b7b8f39a07775994f0
Jeffersson <barrosjefferson407@gmail.com>
2024-12-17 17:15:40 -0300

    Remoção do ListAPIView

--- django/users/views.py
+++ django/users/views.py
@@ -8,11 +8,6 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class UserListAPIView(generics.ListAPIView):
-    queryset = User.objects.all().order_by("id")
-    serializer_class = UserSerializer
-
-
 class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
