46ce071fb99e9dc7382643b985e0f80ced5c4480
Jeffersson <barrosjefferson407@gmail.com>
2024-12-18 19:25:33 -0300

    Autenticação nas apis

--- django/sales/views.py
+++ django/sales/views.py
@@ -1,23 +1,28 @@
 from rest_framework import generics, viewsets
 from sales.models import ProductsSales, Sales
 from sales.serializers import SerializerProductsSales, SerializerSales
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProductsSalesViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = ProductsSales.objects.all().order_by("id")
     serializer_class = SerializerProductsSales
 
 
 class SalesViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Sales.objects.all().order_by("id")
     serializer_class = SerializerSales
 
 
 class SalesDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Sales.objects.all()
     serializer_class = SerializerSales
 
 
 class ProductsSalesDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = ProductsSales.objects.all()
     serializer_class = SerializerProductsSales
--- django/users/views.py
+++ django/users/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics, viewsets
 from users.models import User
 from users.serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all().order_by("id")
     serializer_class = UserSerializer
 
 
 class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
