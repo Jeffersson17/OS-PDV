e405e14ea7cae5b0ce9bd3adcdf7b5e28da1d83e
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2024-12-18 19:26:58 -0300

    Django jwt (#13)
    * Autenticação nas apis
    
    * Mudanças do script

--- django/addresses/views.py
+++ django/addresses/views.py
@@ -1,23 +1,28 @@
 from addresses.models import Address, City
 from addresses.serializers import AddressSerializer, CitySerializer
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 class AddressViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Address.objects.all().order_by("id")
     serializer_class = AddressSerializer
 
 
 class CityViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = City.objects.all().order_by("id")
     serializer_class = CitySerializer
 
 
 class AddressDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
 
 
 class CityDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = City.objects.all()
     serializer_class = CitySerializer
--- django/products/views.py
+++ django/products/views.py
@@ -1,23 +1,28 @@
 from products.models import Product, ProductBrand
 from products.serializers import ProductSerializer, SerializerProductBrand
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all().order_by("id")
     serializer_class = ProductSerializer
 
 
 class ProductBrandViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = ProductBrand.objects.all().order_by("id")
     serializer_class = SerializerProductBrand
 
 
 class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductBrandDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = ProductBrand.objects.all()
     serializer_class = SerializerProductBrand
